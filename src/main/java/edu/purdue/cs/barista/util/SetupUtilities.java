96b4653455d5e111ee15c22886be0628dc927f6d
Andrew Davis <drew@drewdavis.me>
2020-04-29 22:30:58 -0400

    Re-work getPackageToTest method with system properties

--- src/main/java/edu/purdue/cs/barista/util/SetupUtilities.java
+++ src/main/java/edu/purdue/cs/barista/util/SetupUtilities.java
@@ -5,22 +5,33 @@ package edu.purdue.cs.barista.util;
  * test suites.
  *
  * @author Andrew Davis, drew@drewdavis.me
- * @version 1.3, 02/03/2020
+ * @version 2.0, 04/29/2020
  * @since 1.3
  */
 public class SetupUtilities {
 
     /**
-     * Returns the package to test, defined by the `BARISTA_TEST_PACKAGE`
-     * environment variable.
+     * Returns the package to test. This is determined by
+     * the {@code environment} system property. If the
+     * {@code environment} property is set to {@code development},
+     * the solution package, defined by the {@code solution.package.name}
+     * system property, is returned, followed by a terminating period.
+     * If the {@code environment} property is set to
+     * {@code production} or anything but {@code development},
+     * an empty {@link String} is returned, signifying the
+     * {@code default} package.
      *
      * @return the name of the package to test
      */
     public static String getPackageToTest() {
-        String packageName = System.getenv("BARISTA_TEST_PACKAGE");
-        if (packageName == null) packageName = "";
-        else packageName += ".";
-        return packageName;
+        String environment = System.getProperty("environment", "production");
+
+        if (environment.equals("development")) {
+            String solutionPackage = System.getProperty("solution.package.name", "solution");
+            return solutionPackage + ".";
+        } else {
+            return "";
+        }
     }
 
 }
